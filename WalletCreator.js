aaa9d707793ac10f1f0a8da9203a7edea4f23173
Alberto Mabjaia <52320920+alberto-mabjaia@users.noreply.github.com>
2022-12-24 10:51:31 +0200

    Add files via upload

---
+++ WalletCreator.js
@@ -0,0 +1,34 @@
+// dependencies
+
+const bip32 = require("bip32");
+const bip39 = require("bip39");
+const bitcoin = require("bitcoinjs-lib");
+
+// network
+const network = bitcoin.network.testnet
+
+// address 
+
+const path = `m/49'/1'/0'/0` 
+
+// mnimonic to create seed
+
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//criando a raiz da cartiera HD
+let root = bip32.fromSeed(seed, network)
+
+//creating account- par pvt-pub keys
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address
+
+console.log("Carteira gerada")
+console.log("Endereço: ", btcAddress)
+console.log("Chave privada:", node.toWIF())
+console.log("Seed:", mnemonic)
